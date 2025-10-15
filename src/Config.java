fd09209ee2f381aaa32c601f636f0ab9cb721c53
Dhayanidhi1105 <157275666+Dhayanidhi1105@users.noreply.github.com>
2025-10-15 23:56:24 +0530

    Add Config class for application configuration

---
+++ src/Config.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.logging.LoggingMode;
+
+
+
+public class Config {
+    public static LoggingMode loggingMode=LoggingMode.Production;//Default value
+    public static String Outputformat="Console";
+    public static  String OutputFilePath="";
+}
